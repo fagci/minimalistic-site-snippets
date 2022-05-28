00b99887e906cf698de361b0d3d30005ce518876
Mikhail <fagci.nsk@gmail.com>
2022-05-29 00:39:08 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,9 +1,31 @@
 // current link highlight
 (function() {
   const doc = window.document;
-  const links = doc.links
+  const links = doc.links;
   const linksCount = links.length;
   for (let i = 0; i < linksCount; i++)
     if(doc.URL.startsWith(links[i].href))
-      links[i].classList.add('active')
+      links[i].classList.add('active');
+})();
+// toc
+(function() {
+window.addEventListener('DOMContentLoaded', function() {
+    const toc = document.querySelector('.toc');
+    const h = document.querySelectorAll('h2');
+    for (let i in h) {
+        if (!h.hasOwnProperty(i)) continue;
+      
+        const el = h[i];
+        if (el.dataset.notoc || el.innerText.trim().length < 2) continue;
+        if (!el.id) {
+            el.id = el.dataset.toc || el.innerText;
+        }
+      
+        const link = document.createElement('a');
+        link.innerText = el.dataset.toc || el.innerText;
+        link.classList.add('ym-toc__item');
+        link.href = '#' + el.id;
+        toc.appendChild(link);
+    }
+})
 })();
